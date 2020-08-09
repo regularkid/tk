497fdd034ed5b722d3ba7034a97461bda91d8f97
Bryan Perfetto <regularkid@gmail.com>
2020-08-09 09:22:13 -0700

    Renamed main.js -> exampleGame.js

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ Another JavaScript micro game engine called **Tiknee** (pronounced "Tiny".. but 
     <body>
         <div id="game"></div>
         <script src="tk.js"></script>
-        <script src="myGame.js"></script>
+        <script src="exampleGame.js"></script>
     </body>
 </html>
 ```
@@ -35,7 +35,7 @@ gameHeight = 150;
 gameScale = 3.0;
 ```
 
-3. Make a new JavaScript file (myGame.js in the example above) for all of your game specific code (checkout main.js for an example):
+3. Make a new JavaScript file (exampleGame.js in the example above) for all of your game specific code:
 
 ```
 // Load assets
---
+++ exampleGame.js
@@ -0,0 +1,41 @@
+// Load assets
+star = LoadSprite("Star.png")
+
+// Example state
+ExampleState = (reason) =>
+{
+    // Example: How to handle state enter/exit logic
+    if (reason == Enter)
+    {
+        console.log("Entering 'Example' state");
+    }
+    else if (reason == Exit)
+    {
+        console.log("Exiting 'Example' state");
+    }
+
+    // Example: Log touch position while being held
+    if (touch.hold)
+    {
+        console.log("Touch Pos: " + touch.x + ", " + touch.y);
+    }
+
+    // Example: Change background color depending on where a new touch is pressed
+    if (touch.down)
+    {
+        clearColor = (touch.x < gameWidth*0.5) ? "#888" : "#000";
+    }
+
+    // Example: Play SFX on touch released
+    if (touch.up)
+    {
+        zzfx(...[,,895,,.01,.4,4,4.18,.7,.5,,,,.7,,.7,,.92,.02]);
+    }
+
+    // Example: Ugly/pixelated rendering!
+    DrawSprite(star, 100, 90, 1.0, 1.0, performance.now() * 0.1);
+    DrawText("TK Demo!", 100, 45, 32, "#F80", Math.sin(performance.now() * -0.02)*3.0, "Arial", "Bold", "center", "center");
+}
+
+// Start initial state
+nextState = ExampleState;
